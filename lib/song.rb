1653fd3c0a335e576a2b14eac158bf768fe91eb5
123JackCole <123jack.cole@gmail.com>
2020-01-30 10:43:55 -0800

    Done.

---
+++ lib/song.rb
@@ -0,0 +1,46 @@
+require "pry"
+
+class Song
+
+    @@all = []
+    @@artists = []
+    @@genres = []
+    @@count = 0
+
+    attr_accessor :name, :artist, :genre
+
+    def initialize(name, artist, genre)
+        @name = name
+        @artist = artist
+        @genre = genre
+        @@all << self
+        @@artists << self.artist
+        @@genres << self.genre
+        @@count += 1
+    end
+
+    def self.all
+        @@all.uniq
+    end
+
+    def self.count
+        @@count
+    end
+
+    def self.artists
+        @@artists.uniq
+    end
+
+    def self.genres
+        @@genres.uniq
+    end
+
+    def self.genre_count
+        Hash[*@@genres.group_by{ |v| v }.flat_map{ |k, v| [k, v.size] }]
+    end
+
+    def self.artist_count
+        Hash[*@@artists.group_by{ |v| v }.flat_map{ |k, v| [k, v.size] }]
+    end
+
+end
